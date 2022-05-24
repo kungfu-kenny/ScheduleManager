3deccf301c2e4a9221924fc836b748900e3a9172
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-05-24 17:29:07 +0300

    started working on the creating the data for the databse

--- start.py
+++ start.py
@@ -4,15 +4,15 @@ from parsers.parse_teacher_mmsa import DataTeacherMmsa
 from config import Folders
 
 
-# try:
-os.path.exists(Folders.folder_storage) or os.mkdir(Folders.folder_storage)
-for cls in [
-    DataTeacherSp,
-    DataTeacherMmsa,
-]:
-    a = cls()
-    a.start_parse()
+try:
+    os.path.exists(Folders.folder_storage) or os.mkdir(Folders.folder_storage)
+    for cls in [
+        DataTeacherSp,
+        DataTeacherMmsa,
+    ]:
+        a = cls()
+        a.start_parse()
 
-# except Exception as e:
-#     print(e)
-#     print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+except Exception as e:
+    print(e)
+    print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
---
+++ utilities/develop_csv.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from config import (
+    Keys,
+    IasaSP,
+    IasaMMSA,
+)
+
+#TODO add work from here
+class DevelopAdditionalCSV:
+    """
+    class which is dedicated to develop additional csv for the databases
+    """
+    def __init__(self) -> None:
+        pass
+
+    def parse_specialization(self) -> None:
+        """
+        
+        """
+        pass
+
+    def parse_subjects(self) -> None:
+        """
+        
+        """
+        pass
+
+    def parse_directions(self) -> None:
+        """
+        
+        """
+        pass
+
+    def parse_database(self) -> None:
+        """
+        Method which is dedicated to create csv values for the database values
+        and use them after to the database usage
+        Input:  None
+        Output: we created to df for the comfortable parsings
+        """
+        pass
+
+    def parse_main(self) -> None:
+        """
+        Method which is dedicated to create the csv values for the all of it
+        Input:  None
+        Output: we created the csv values for the additional values
+        """
+        pass
