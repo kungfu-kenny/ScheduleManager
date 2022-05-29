0c6ed332358a69a1434a8b9e529899a4323101cd
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-05-30 01:03:35 +0300

    continued working on the database values and develop the values

--- models/models_db.py
+++ models/models_db.py
@@ -1,14 +1,17 @@
+from datetime import datetime
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import (create_engine,
                         Table,
                         Column,
+                        Boolean,
                         Integer,
                         String,
+                        DateTime,
                         ForeignKey,
                         PrimaryKeyConstraint)
 
-
+#TODO continue from here to work with the selected database and it's insertion
 Base = declarative_base()
 
 association_faculty_chair = Table(
@@ -51,6 +54,20 @@ association_teacher_specialization = Table(
     PrimaryKeyConstraint("id_teacher", "id_specialization"),
 )
 
+association_teacher_subject_specialization = Table(
+    "teacher_subject_specialization",
+    Base.metadata,
+    Column("id_teacher", ForeignKey('teacher.id')),
+    Column("id_subject", ForeignKey("subject.id")),
+    Column("id_specialization", ForeignKey("specialization.id")),
+    Column("hours_used", Integer, default=40),
+    Column("semester_start", Integer, default=1),
+    Column("allow_split", Boolean, default=True),
+    Column("masters", Boolean, default=True),
+    Column('date_inserted', DateTime, default=datetime.now()),
+    PrimaryKeyConstraint("id_teacher", "id_subject", "id_specialization"),
+)
+
 class Chair(Base):
     __tablename__ = 'chair'
     id = Column(Integer, primary_key=True)
@@ -77,6 +94,16 @@ class Faculty(Base):
         secondary=association_faculty_building, 
         back_populates="faculty"
     )
+
+class Group(Base):
+    __tablename__ = 'group'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150))
+    year_started = Column(Integer, default=datetime.now().year)
+    chair = Column(Integer, ForeignKey('chair.id'))
+    faculty = Column(Integer, ForeignKey('faculty.id'))
+    specialization = Column(Integer, ForeignKey('specialization.id'))
+    building_default = Column(Integer, ForeignKey('building.id'))
 
 class Teacher(Base):
     __tablename__ = 'teacher'
